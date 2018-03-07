95c4068f17f93f3c6f6abd5e448d2dfb1d8aaa30
afeiship <1290657123@qq.com>
2018-03-07 13:52:38 +0800

    issue: optmize

--- views/home.js
+++ views/home.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 
 export default class Home extends Component {
-    render() {
-        return (
-            <html>
-                <head>
-                    <title>{this.props.title}</title>
-                </head>
-                <body>
-                    <h1>{this.props.title}</h1>
-                    <p className="test">Page rendered with {this.props.viewEngine}
-                        view engine.</p>
-                </body>
-            </html>
-        );
-    }
+  render() {
+    return (
+      <html>
+        <head>
+          <title>{this.props.title}</title>
+        </head>
+        <body>
+          <h1>{this.props.title}</h1>
+          <p className="test">Page rendered with {this.props.viewEngine}
+            view engine.</p>
+        </body>
+      </html>
+    );
+  }
 };
